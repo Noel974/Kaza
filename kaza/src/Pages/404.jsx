7661385b9f83b0d411042c6091e801de2ab1960c
noel <noelantoine974@outlook.fr>
2024-02-28 20:11:43 +0400

    erro

--- kaza/src/Pages/404.jsx
+++ kaza/src/Pages/404.jsx
@@ -8,7 +8,7 @@ export default function E404(){
         <div className="page_404">
 			<div className="notFound_infos">
 				<h1 className='notFound_infos_title'>404</h1>
-				<p className='notFound_infos_content'>La page que vous recherchez n'existe pas.</p>
+				<p className='notFound_infos_content'>Oups! La page que vous demandez n'existe pas.</p>
 			</div>
 				<Link className='notFound_infos_return' to='/'>Retourner sur la page d'accueil</Link>
         </div>
