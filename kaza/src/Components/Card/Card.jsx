de3a6f6e3bde528b1267709ca8d43f6353f786cb
noel <noelantoine974@outlook.fr>
2024-03-10 18:50:41 +0400

    mise a jour fiche logement ajout des compenets

--- kaza/src/Components/Card/Card.jsx
+++ kaza/src/Components/Card/Card.jsx
@@ -1,17 +1,12 @@
 import datas from "../../assets/data/data.json";
-import { Link } from 'react-router-dom';
+import CardItem from "./Carditem";
 
 export default function Card() {
     return (
         <main className='home_card'>
             {datas.map(data => {
                 console.log("Data ID:", data.id);
-                return (
-                    <Link to={`/Fichelogement/${data.id}`} className="home_gallery" key={data.id}>
-                        <img src={data.cover} alt={data.title} />
-                        <h3>{data.title}</h3>
-                    </Link>
-                )
+                return <CardItem key={data.id} id={data.id} cover={data.cover} title={data.title} />;
             })}
         </main>
     )
