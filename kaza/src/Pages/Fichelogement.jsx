e39f6a2b8c45b43b8e9b99dbb747c03c1e7512d4
noel <noelantoine974@outlook.fr>
2024-03-16 15:10:14 +0400

    mise a jour de la fiche logment;2

--- kaza/src/Pages/Fichelogement.jsx
+++ kaza/src/Pages/Fichelogement.jsx
@@ -28,33 +28,31 @@ export default function Fichelogement() {
         console.log('Rendering 404 page');
         return <E404 />;
     }
-
-    const description = dataCurrentLogment[0].description;
-    const equipments = dataCurrentLogment[0].equipments;
-
+    
     return (
         <>
             <Carrousel imageSlider={imageSlider} />
             <main className="Logment">
                 <div className="infologment">
-                <Infologement 
-                        title={dataCurrentLogment[0].title} 
-                        location={dataCurrentLogment[0].location} 
-                        tags={dataCurrentLogment[0].tags} 
-                        host={dataCurrentLogment[0].host} 
-                        rating={dataCurrentLogment[0].rating} 
+                    <Infologement
+                        title={dataCurrentLogment[0].title}
+                        location={dataCurrentLogment[0].location}
+                        tags={dataCurrentLogment[0].tags}
+                        host={dataCurrentLogment[0].host}
+                        rating={dataCurrentLogment[0].rating}
                     />
                 </div>
                 <div className="Logment_infopanel">
                     <div className='detail_info'>
                         {/* Passer la description en tant que prop 'contenu' */}
-                        <Infopanel title={'Description'} content={description} />
+                        <Infopanel title={'Description'} content={dataCurrentLogment[0].description} />
                     </div>
                     <div className='detail_info'>
                         {/* Passer les équipements en tant que prop 'contenu' */}
-                        <Infopanel title={'Équipements'} content={equipments} className="equipments" />
+                        <Infopanel title={'Équipements'} content={dataCurrentLogment[0].equipments} className="equipments" />
                     </div>
                 </div>
+
             </main>
         </>
     );
